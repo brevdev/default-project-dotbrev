384f7ce841c7c9eee6d4347c8a0b2d58e7b0b304
Ali <ali.ahmed@berkeley.edu>
2021-09-16 10:47:45 -0700

    Update setup.sh

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -6,35 +6,31 @@ set -euo pipefail
 ##### Specify software and dependencies that are required for this project     #####
 #####                                                                          #####
 ##### Note:                                                                    ##### 
-##### (1) A log file is auto-created when this file runs. If you want to write #####
-##### to it, the relative path is ./.brev/logs/setup.log. By default, all      #####
-##### stderr and stdout from this file are sent there.                         #####
-#####                                                                          #####
-##### (2) The working directory is /home/brev/<PROJECT_FOLDER_NAME>. Execution #####
-##### of this file happens at this level.                                      #####
+##### The working directory is /home/brev/<PROJECT_FOLDER_NAME>. Execution of  #####
+##### this file happens at this level.                                         #####
 ####################################################################################
 
 ##### Yarn #####
-# (echo ""; echo "##### Yarn #####"; echo "";) >> ./.brev/logs/setup.log
+# (echo ""; echo "##### Yarn #####"; echo "";)
 # curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add
 # echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 # sudo apt update
 # sudo apt install -y yarn
 
 ##### Homebrew #####
-# (echo ""; echo "##### Homebrew #####"; echo "";) >> ./.brev/logs/setup.log
+# (echo ""; echo "##### Homebrew #####"; echo "";)
 # curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash -
 # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/brev/.bash_profile
 # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/brev/.zshrc
 # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 ##### Node v14.x + npm #####
-# (echo ""; echo "##### Node v14.x + npm #####"; echo "";) >> ./.brev/logs/setup.log
+# (echo ""; echo "##### Node v14.x + npm #####"; echo "";)
 # curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 
 ##### Python + Pip + Poetry #####
-# (echo ""; echo "##### Python + Pip + Poetry #####"; echo "";) >> ./.brev/logs/setup.log
+# (echo ""; echo "##### Python + Pip + Poetry #####"; echo "";)
 # sudo apt-get install -y python3-distutils
 # sudo apt-get install -y python3-apt
 # curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
@@ -44,7 +40,7 @@ set -euo pipefail
 # source $HOME/.poetry/env
 
 ##### Golang v16x #####
-# (echo ""; echo "##### Golang v16x #####"; echo "";) >> ./.brev/logs/setup.log
+# (echo ""; echo "##### Golang v16x #####"; echo "";)
 # wget https://golang.org/dl/go1.16.7.linux-amd64.tar.gz
 # sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.16.7.linux-amd64.tar.gz
 # echo "" | sudo tee -a ~/.bashrc
@@ -56,5 +52,5 @@ set -euo pipefail
 # rm go1.16.7.linux-amd64.tar.gz
 
 ##### Custom commands #####
-# (echo ""; echo "##### Custom commands #####"; echo "";) >> ./.brev/logs/setup.log
+# (echo ""; echo "##### Custom commands #####"; echo "";)
 # npm install
