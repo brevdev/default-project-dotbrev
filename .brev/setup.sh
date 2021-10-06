7a4a7027d4b7ea740841922802ae8769a5161717
Ali <ali.ahmed@berkeley.edu>
2021-10-06 13:10:32 -0700

    Update setup.sh

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -26,6 +26,7 @@ set -euo pipefail
 
 ##### Node v14.x + npm #####
 # (echo ""; echo "##### Node v14.x + npm #####"; echo "";)
+# sudo apt install ca-certificates
 # curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # sudo apt-get install -y nodejs
 
