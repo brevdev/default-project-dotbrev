4e0d88be9324331a840930defede5678df51d1c8
r33drichards <57335981+r33drichards@users.noreply.github.com>
2021-11-16 01:40:01 +0000

    script to install npm packages globally without sudo

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 ####################################################################################
 ##### Specify software and dependencies that are required for this project     #####
 #####                                                                          #####
-##### Note:                                                                    ##### 
+##### Note:                                                                    #####
 ##### The working directory is /home/brev/<PROJECT_FOLDER_NAME>. Execution of  #####
 ##### this file happens at this level.                                         #####
 ####################################################################################
@@ -29,6 +29,20 @@ set -euo pipefail
 # sudo apt install ca-certificates
 # curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 # sudo apt-get install -y nodejs
+
+# install npm packages globally without sudo
+# modified from https://stackoverflow.com/questions/18088372/how-to-npm-install-global-not-as-root
+# mkdir "${HOME}/.npm-packages"
+# printf "prefix=${HOME}/.npm-packages" >> $HOME/.npmrc
+# cat <<EOF | tee -a ~/.bashrc | tee -a ~/.zshrc
+# NPM_PACKAGES="\${HOME}/.npm-packages"
+# NODE_PATH="\${NPM_PACKAGES}/lib/node_modules:\${NODE_PATH}"
+# PATH="\${NPM_PACKAGES}/bin:\${PATH}"
+# # Unset manpath so we can inherit from /etc/manpath via the `manpath`
+# # command
+# unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+# MANPATH="\${NPM_PACKAGES}/share/man:\$(manpath)"
+# EOF
 
 ##### Python + Pip + Poetry #####
 # (echo ""; echo "##### Python + Pip + Poetry #####"; echo "";)
